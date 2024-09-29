eb5449c2da79c4c6cce677a8de9d4ce1aa7478ca
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-29 23:59:58 +0300

    Initial Commit

---
+++ AsyncImageSwiftUI/AsyncImageSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftUIApp.swift
+//  AsyncImageSwiftUI
+//
+//  Created by Beyza Nur Tekerek on 29.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
