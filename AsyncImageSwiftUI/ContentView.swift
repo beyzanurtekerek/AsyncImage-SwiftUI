9097791a2c09acce2c0d92bcd69550f583de7141
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-30 00:19:11 +0300

    first commit

--- AsyncImageSwiftUI/ContentView.swift
+++ AsyncImageSwiftUI/ContentView.swift
@@ -9,13 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView {
+            
+            VStack {
+                
+                AsyncImage(url: URL(string: "https://cdn.sanity.io/images/tlr8oxjg/production/e91a4659d80f9de0294d1fc4d9c78b23a4e93146-1456x816.png?w=3840&q=100&fit=clip&auto=format")!) { image in
+                    image.resizable().frame(width: 300, height:200, alignment: .center)
+                } placeholder: {
+                    // görsel indirilene kadar ne gösterilsin
+                    ProgressView()
+                }
+                
+                List(superHeroArray) {superhero in
+                    Text(superhero.name)
+                        .font(.title3)
+                        .foregroundColor(.brown)
+                }
+            }
+            .navigationTitle("Superhero Book")
         }
-        .padding()
     }
 }
 
